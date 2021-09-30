06e13718be3217bb9a789e6b681e07b551fe932c
Nbsh18 <nbsh18@student.aau.dk>
2021-09-30 21:28:41 +0200

    Added objects

--- Assets/Badminton/Shuttlecock/MoveCurve.cs
+++ Assets/Badminton/Shuttlecock/MoveCurve.cs
@@ -6,13 +6,14 @@ public class MoveCurve : MonoBehaviour
 
     // Update is called once per frame
     public float t = 0.0f, velocity = 0.0f;
-    public Vector3 curveTowards, endPoint;
+    public Vector3 curveTowards;
+    public GameObject Obj1;
 
     void Update()
     {
         t += velocity * Time.deltaTime;
         if (t > 1.0) { t = 1.0f; }
-        transform.position = (1.0f - t) * (1.0f - t) * transform.position + 2 * (1.0f - t) * t * curveTowards + (t * t) * endPoint;
+        transform.position = (1.0f - t) * (1.0f - t) * transform.position + 2 * (1.0f - t) * t * curveTowards + (t * t) * Obj1.transform.position;
 
     }
 }
