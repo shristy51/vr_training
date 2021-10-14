653814c728b2f1dd6bc1eeb54804f3f5f12c75ff
Shristy Shah <shristyshah@h229.aau1x.klient.njv12.site.aau.dk>
2021-10-14 13:53:43 +0200

    add all animation clips

---
+++ Assets/Animation/XRHandController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public enum HandType
+{
+    Left,
+    Right
+}
+
+public class XRHandController : MonoBehaviour
+{
+    public HandType handType;
+
+    private Animator animator;
+    private InputDevice inputDevice;
+
+    private float fistValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        inputDevice = GetInputDevice();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AnimateHand();
+    }
+
+    InputDevice GetInputDevice()
+    {
+        InputDeviceCharacteristics controllerCharacteristic = InputDeviceCharacteristics.HeldInHand | InputDeviceCharacteristics.Controller;
+
+        if (handType == HandType.Left)
+        {
+            controllerCharacteristic = controllerCharacteristic | InputDeviceCharacteristics.Left;
+        }
+        else
+        {
+            controllerCharacteristic = controllerCharacteristic | InputDeviceCharacteristics.Right;
+        }
+
+        List<InputDevice> inputDevices = new List<InputDevice>();
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristic, inputDevices);
+
+        return inputDevices[0];
+    }
+
+    void AnimateHand()
+    {
+        inputDevice.TryGetFeatureValue(CommonUsages.grip, out fistValue);
+        animator.SetFloat("Grab", fistValue);
+    }
+}
