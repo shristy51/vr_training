085bda5b018aa93558a2f3739fa6da12b4f13fd0
Nbsh18 <nbsh18@student.aau.dk>
2021-11-16 15:10:31 +0100

    score board attempt

--- Assets/Badminton/Racquet/RacquetPhysics.cs
+++ Assets/Badminton/Racquet/RacquetPhysics.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class RacquetPhysics : MonoBehaviour
 {
-
+	static int score_counter=-1;
+	public List<GameObject> Score = new List<GameObject>();
     private void OnTriggerEnter(Collider other)
 	{
 		GameObject shuttle = other.gameObject;
-		//Debug.Log("Collision detected" + other.gameObject);
+		
+		//	Debug.Log("Collision detected" + other.gameObject);
 		if(shuttle.CompareTag("shuttle"))
 		{
-			
+			score_counter++;
+			//Score[score_counter]
 			Debug.Log("Destroying " + other.gameObject);
 			
 		}
